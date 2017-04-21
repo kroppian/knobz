efbe9fde24378ea6489a4101349bf8825cc16e5d
Ian Kropp <iankropp@gmail.com>
2017-04-21 16:21:45 -0400

    Upping the knob count to six

--- src/c/main.c
+++ src/c/main.c
@@ -6,16 +6,31 @@
 #include <avr/interrupt.h>
 #include "libuart.h"
 
-#define CHANNEL_1 0
-#define CHANNEL_2 (1 << MUX0)
+#define CHANNEL_0 0
+#define CHANNEL_1 (1 << MUX0)
+#define CHANNEL_2 (1 << MUX1)
+#define CHANNEL_3 (1 << MUX0) | (1 << MUX1)
+#define CHANNEL_4 (1 << MUX2)
+#define CHANNEL_5 (1 << MUX2) | (1 << MUX0)
 #define ADMUX_DEFAULT (1 << REFS0) | (1 << ADLAR)
+
+// 0000
+// 0001
+// 0010
+// 0011
+// 0100
 
 volatile unsigned char receivedByte;
 
 int main()
 {
+
   DDRB |= 1 << PINB1;
 
+  for(int j = 0; j < 4; j++){
+    PORTB ^= 1 << PINB1;
+    _delay_ms(300);
+  }
 
   // 1. baud rate
   // 2. async dub speed
@@ -54,13 +69,29 @@ ISR(ADC_vect){
   int myNum = ADCH;
 
   switch(ADMUX){
-    case (ADMUX_DEFAULT | CHANNEL_1): 
-      ADMUX = ADMUX_DEFAULT | CHANNEL_2;
+    case (ADMUX_DEFAULT | CHANNEL_0): 
+      ADMUX = ADMUX_DEFAULT | CHANNEL_1;
       uart_send(0x0F);  
       uart_send(myNum); 
       break;
+    case (ADMUX_DEFAULT | CHANNEL_1): 
+      ADMUX = ADMUX_DEFAULT | CHANNEL_2;
+      uart_send(myNum);  
+      break;
     case (ADMUX_DEFAULT | CHANNEL_2): 
-      ADMUX = ADMUX_DEFAULT | CHANNEL_1;
+      ADMUX = ADMUX_DEFAULT | CHANNEL_3;
+      uart_send(myNum);  
+      break;
+    case (ADMUX_DEFAULT | CHANNEL_3): 
+      ADMUX = ADMUX_DEFAULT | CHANNEL_4;
+      uart_send(myNum);  
+      break;
+    case (ADMUX_DEFAULT | CHANNEL_4): 
+      ADMUX = ADMUX_DEFAULT | CHANNEL_5;
+      uart_send(myNum);  
+      break;
+    case (ADMUX_DEFAULT | CHANNEL_5): 
+      ADMUX = ADMUX_DEFAULT | CHANNEL_0;
       uart_send(myNum);  
       uart_send(0xF0);  
       break;
